be412d8107abd85d8429ffd1a7dfc932fda7eaa4
Glitch (q1-2nd-graded-exercise-calawodsodium) <none>
2024-09-14 14:53:34 +0000

    ⏏️🔻 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,11 @@
+// This gets the user's name
+
+var username1 = prompt("Please enter your name:");
+
+var username2 = prompt("Please enter your name:");
+
+// These fill up the table cells with the name and greetings
+person1.innerHTML = username1;
+person2.innerHTML = username2;
+greeting1.innerHTML = "Hi " + username1;
+greeting2.innerHTML = "Hi " + username2;
